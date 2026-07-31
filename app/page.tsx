18dd7ea1496828c718a7729d1785ada642070a81
Sjuejs88s <hhhabc36@gmail.com>
2026-08-01 01:18:41 +0300

    regester

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,13 @@
 
 export default function Home() {
   return (
-   <>
-   <button>add</button>
-   </>
+   <div className = "container">
+    <h1>creat acount</h1>
+    <input type="text" placeholder="Enter Your name" required />
+   <input type="number" placeholder=" phone number" required />
+   <input type="password" placeholder="Enter Your password" required />
+  <input type="password" placeholder="Confirm Your password" required />
+  <button type="submit" className="b">Login</button>
+   </div>
   );
 }
