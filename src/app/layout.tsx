a9d51128a2123505e3c7f1afdfc39b69b2e3d4ab
Ali Mahdi <ali.mahdi.67.99@gmail.com>
2026-08-06 02:38:46 +0300

    update logo Header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
   type: "website",
   images: [
     {
-      url: "/logo.png",
+      url: "/logoHeader.jpg",
       width: 1200,
       height: 630,
       alt: "TamraLine",
